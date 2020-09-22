7943873264b8aa168626486d32fe02af0773ee09
Josh <jemidlo17@gmail.com>
2020-09-22 10:55:09 -0500

    added character list

--- dnd/src/app/app.module.ts
+++ dnd/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NewCharComponent } from './new-char/new-char.component';
 import { MonsterFormComponent } from './monster-form/monster-form.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { CampaignFormComponent } from './campaign-form/campaign-form.component';
+import { CharactersComponent } from './characters/characters.component';
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import { CampaignFormComponent } from './campaign-form/campaign-form.component';
     MonsterFormComponent,
     NavigationComponent,
     CampaignFormComponent,
+    CharactersComponent,
   ],
   imports: [
     BrowserModule,
