08c7f5afd58d363be76e49587b1a566d48020ac5
Bekrija Dervisevic <BubbaReppas@gmail.com>
2020-09-22 13:12:03 -0500

    adding basic routes to DM and new campaign

--- dnd/src/app/app-routing.module.ts
+++ dnd/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DmPageComponent } from './dm-page/dm-page.component';
+import { CampFormPageComponent } from './camp-form-page/camp-form-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'DungeonMaster', component: DmPageComponent },
+  { path: 'Campaigns/new', component: CampFormPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
