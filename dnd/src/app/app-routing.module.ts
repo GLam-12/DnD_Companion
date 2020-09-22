130cb1ea1186109350b5c2b7cac07dc85edca019
Josh <jemidlo17@gmail.com>
2020-09-22 13:17:48 -0500

    adding routes

--- dnd/src/app/app-routing.module.ts
+++ dnd/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharFormPageComponent } from './char-form-page/char-form-page.component';
+import { CharPageComponent } from './char-page/char-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'character', component: CharPageComponent},
+  {path: 'create', component: CharFormPageComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
