809f94aa8194e2f6a6012b98137820bea59618d5
Jerry Perkins <renrag27@yahoo.com>
2020-09-22 13:18:48 -0500

    put small components into player and login pages

--- dnd/src/app/app-routing.module.ts
+++ dnd/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginRegPageComponent } from './login-reg-page/login-reg-page.component';
+import { PlayerPageComponent } from './player-page/player-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login_registration', component: LoginRegPageComponent},
+  {path: 'player_page', component: PlayerPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
