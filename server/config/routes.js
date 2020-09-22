2440aa3b27ae0b95f02da32dedd0dbacf8df0e67
Jerry Perkins <renrag27@yahoo.com>
2020-09-22 15:17:54 -0500

    server, controller and config added

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,1 +1,32 @@
-//hello this is here
+const users = require('../controllers/users')
+
+module.exports = function(app, path){
+    app.get('/', (req, res) =>{
+
+    })
+
+    app.get('/user', (req, res) => {
+        users.all_users(req, res)
+    })
+
+    app.get('/user/:id', (req, res) => {
+        users.one_user(req, res)
+    })
+
+    app.post('/user', (req, res) => {
+        users.add_user(req, res)
+    })
+
+    app.put('/user/:id', (req, res) =>{
+        users.edit_user(req, res)
+    })
+
+    app.delete('/user/:id', (req, res) => {
+        pets.delete_user(req, res)
+    })
+
+    app.all('*', (req, res) =>{
+        res.sendFile(path + '/index.html')
+    })
+}
+
