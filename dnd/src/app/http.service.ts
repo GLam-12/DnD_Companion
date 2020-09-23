47ff0819e3f24c70bb96d5c80f0b4a2597165d1f
Jerry Perkins <renrag27@yahoo.com>
2020-09-23 15:00:40 -0500

    live dice

--- dnd/src/app/http.service.ts
+++ dnd/src/app/http.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as io from 'socket.io-client'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-
+  // socket: any
   constructor(private _http: HttpClient) { }
 
   add_user(new_user){
     return this._http.post('/user', new_user)
   }  
+
+  setupsocket(){
+    return io('http://Localhost:8000')
+  }
 }
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const { Socket } = require('dgram');
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
 
 require('./server/config/mongoose')
 
@@ -8,8 +12,36 @@ app.use(express.static(__dirname + "/dnd/dist/dnd"))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
+io.on('connection', (socket) =>{
+    console.log('user connected')
+    socket.emit("welcome", "welcome to the server")
+    socket.on('roll_20', (data)=>{
+        console.log('20 sided rand int received', data)
+        io.emit('roll_20 to share', data)
+    })
+    socket.on('roll_6', (data )=> {
+        console.log('6 sided dice received', data)
+        io.emit('roll_6 to share', data)
+    })
+    socket.on('roll_4', (data) =>{
+        console.log('4 sided dice received', data)
+        io.emit('roll_4 to share', data)
+    })
+    socket.on('roll_12', (data) =>{
+        console.log('12 sided dice received', data)
+        io.emit('roll_12 to share', data)
+    })
+    socket.on('roll_10', (data)=>{
+        console.log('10 sided dice received', data)
+        io.emit('roll_10 to share', data)
+    })
+    socket.on('roll_8', (data)=>{
+        console.log('8 sided dice received', data)
+        io.emit('roll_8 to share', data)
+    })
+})
 
 require('./server/config/routes')(app, __dirname + "/dnd/dist/dnd")
 
 
-app.listen(8000, () => console.log("Listening on port 8000"))
+http.listen(8000, () => console.log("Listening on port 8000"))
